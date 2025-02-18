f0113557b1d5142d9b1f1cc4ca006dc0d96a02e6
Jesus <jesusmirandaa55@gmail.com>
2025-02-17 22:52:41 -0700

    Add functionality to tank and sentry enemy
    Tank enemies have a small movement boost and sentry enemies shoot at player.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,9 @@ public class Enemy : MonoBehaviour
     public int health = 1;
     public int points = 0;
     private GameObject player;
+    private bool isMovementBoostActive = false;
+    private bool sentryCanShoot = false;
+    public EnemyBullet enemyBullet;
 
     void Start()
     {
@@ -22,5 +25,41 @@ public class Enemy : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
         transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (!isMovementBoostActive) 
+        {
+            StartCoroutine(MovementBoost());
+        }
+
+        if (!sentryCanShoot)
+        {
+            StartCoroutine(SentryShoot());
+        }
+    }
+
+    private IEnumerator MovementBoost()
+    {
+        if (gameObject.name == "TankEnemy(Clone)")
+        {
+            isMovementBoostActive = true;
+            enemySpeed = 1.25f;
+            yield return new WaitForSeconds(0.25f);
+            enemySpeed = 0.75f;
+            yield return new WaitForSeconds(0.25f);
+            isMovementBoostActive = false;
+        }
+
+    }
+
+    private IEnumerator SentryShoot()
+    {
+        if (gameObject.name == "SentryEnemy(Clone)")
+        {
+            sentryCanShoot = true;
+            EnemyBullet bullet = Instantiate(enemyBullet, gameObject.transform.position + gameObject.transform.up * 0.25f, Quaternion.Euler(0, 0, Random.Range(-360f, 360f)));
+            bullet.rigidBody.AddForce(gameObject.transform.up * 1.5f, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(2.5f);
+            sentryCanShoot = false;
+        }
     }
 }
---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public GameObject enemyBullet;
+    public Rigidbody2D rigidBody;
+    public SpriteRenderer enemyBulletSprite => GetComponent<SpriteRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Top Boundary" || 
+            collision.gameObject.name == "Bottom Boundary" || 
+            collision.gameObject.name == "Left Boundary" || 
+            collision.gameObject.name == "Right Boundary")
+        {
+            Destroy(enemyBullet);
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(enemyBullet);
+            Debug.Log("Player hit by enemy bullet");
+            // TODO: Subtract 1 from player health
+        }
+    }
+}
